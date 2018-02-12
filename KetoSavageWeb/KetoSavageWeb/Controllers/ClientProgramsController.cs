ee5ecaab955a88c1052d03445329fca6546d8241
Michael Jensen <mjensen@razoredgetech.com>
2018-02-12 13:04:40 -0700

    Theme
    Updated Theme and added client menu

---
+++ KetoSavageWeb/KetoSavageWeb/Controllers/ClientProgramsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KetoSavageWeb.Controllers
+{
+    public class ClientProgramsController : Controller
+    {
+        // GET: ClientPrograms
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
