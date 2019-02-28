c8807136611f31a4b0cb36464ee5b661539ee427
Michael Jensen <michaelj@savageservices.com>
2019-02-28 14:48:39 -0700

    Still tweaking the score.

--- KetoSavageWeb/KetoSavageWeb/Controllers/ChartsAPIController.cs
+++ KetoSavageWeb/KetoSavageWeb/Controllers/ChartsAPIController.cs
@@ -4,6 +4,7 @@ using KetoSavageWeb.Models;
 using KetoSavageWeb.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -45,17 +46,16 @@ namespace KetoSavageWeb.Controllers
         [HttpGet]
         public HttpResponseMessage GetComplianceScore(DataSourceLoadOptions loadOptions, string type, bool lifetime)
         {
+
             var date = DateTime.Now.Date;
             var weekOfYear = _context.DateModels.Where(x => x.Date == date).Select(y => y.ISOWeekOfYear).First();
 
             var clientProgress = _context.DailyProgress.Where(x => x.UserProgram.IsActive && !x.UserProgram.IsDeleted);
 
-
             if (!lifetime)
             {
-                clientProgress = clientProgress.Where(x => x.Dates.ISOWeekOfYear == weekOfYear);
+                clientProgress = clientProgress.Where(x => x.Dates.ISOWeekOfYear == weekOfYear && x.Dates.Year == date.Year);
             }
-
 
             var scoreModel = returnVarianceScore(clientProgress);
 
@@ -67,8 +67,6 @@ namespace KetoSavageWeb.Controllers
             {
                 scoreModel = scoreModel.Where(x => x.Score <= 5).ToList();
             }
-
-            scoreModel.Take(5);
 
             return Request.CreateResponse(DataSourceLoader.Load(scoreModel, loadOptions));
 
@@ -88,37 +86,59 @@ namespace KetoSavageWeb.Controllers
                     x.ActualProtein,
                     ActualCarbohydrate = (x.ActualCarbohydrate > x.PlannedCarbohydrate) ? x.ActualCarbohydrate : x.PlannedCarbohydrate
                 })
-                .GroupBy(s => new { s.Name })
-                .Select(g => new
+                .ToList()
+                .Select(y => new
                 {
-                    ClientName = g.Key.Name,
-                    FatVariance = (g.Average(x => x.ActualFat - x.PlannedFat)) * 9,
-                    ProtVariance = (g.Average(x => x.ActualProtein - x.PlannedProtein)) * 4,
-                    CarbVariance = (g.Average(x => x.ActualCarbohydrate - x.PlannedCarbohydrate)) * 4
+                    y.Name,
+                    FatVariance = Math.Abs(Convert.ToDouble(y.ActualFat - y.PlannedFat)) * 9,
+                    ProteinVariance = Math.Abs(Convert.ToDouble(y.ActualProtein - y.PlannedProtein)) * 4,
+                    CarbVariance = Math.Abs(Convert.ToDouble(y.ActualCarbohydrate - y.PlannedCarbohydrate)) * 4
                 })
-                .GroupBy(t => new { t.ClientName })
+                .GroupBy(s => new { s.Name })
                 .Select(su => new
                 {
-                    su.Key.ClientName,
-                    TotalVariance = su.Sum(x => x.FatVariance + x.ProtVariance + x.CarbVariance)
+                    su.Key.Name,
+                    TotalVariance = su.Average(x => x.FatVariance + x.ProteinVariance + x.CarbVariance),
+                    Fat = su.Sum(x => x.FatVariance),
+                    Protein = su.Sum(x => x.ProteinVariance),
+                    Carbs = su.Sum(x => x.CarbVariance)
                 });
+                //.GroupBy(s => new { s.Name })
+                //.Select(g => new
+                //{
+                //    ClientName = g.Key.Name,
+                //    FatVariance = (g.Average(x => Math.Abs(Convert.ToDouble(x.ActualFat - x.PlannedFat)))) * 9,
+                //    ProtVariance = (g.Average(x => Math.Abs(Convert.ToDouble(x.ActualProtein - x.PlannedProtein)))) * 4,
+                //    CarbVariance = (g.Average(x => Math.Abs(Convert.ToDouble(x.ActualCarbohydrate - x.PlannedCarbohydrate)))) * 4
+                //})
+                //.GroupBy(t => new { t.ClientName })
+                //.Select(su => new
+                //{
+                //    su.Key.ClientName,
+                //    TotalVariance = su.Sum(x => x.FatVariance + x.ProtVariance + x.CarbVariance)
+                //});
             List<PerformanceChart> chart = new List<PerformanceChart>();
-            foreach(var u in data)
+            foreach(var u in data.Take(5))
             {
-                
+                Debug.WriteLineIf(u.Name == "Michael Jensen", ($"--chart data u value: {u}"));
+
                 var score = returnScore(Math.Abs(Convert.ToInt32(u.TotalVariance)));
+
                 chart.Add(new PerformanceChart()
                 {
-                    ClientName = u.ClientName,
+                    ClientName = u.Name,
                     Score = score
                 });
             }
+
             
             return chart;
         }
 
         private int returnScore(int totalVariance)
         {
+            Debug.WriteLine($"returnScore-----------");
+            Debug.WriteLine($"TotalVariance: {totalVariance}");
             return (totalVariance == 0 ) ? 0 :
                 (totalVariance < 20) ? 10 :
                 (totalVariance < 40) ? 9 :
